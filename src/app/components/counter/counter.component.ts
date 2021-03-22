280508da12eb8ad5fd63baac1f52ece98c56c80d
Jeffry Gonzalez <jeff@hypertheory.com>
2021-03-22 16:55:02 -0400

    done with monday!

--- src/app/components/counter/counter.component.ts
+++ src/app/components/counter/counter.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
-import { AppState } from 'src/app/reducers';
-
+import { AppState, selectCounterCurrent } from 'src/app/reducers';
+import * as actions from '../../actions/counter.actions';
 @Component({
   selector: 'app-counter',
   templateUrl: './counter.component.html',
@@ -16,13 +16,20 @@ export class CounterComponent implements OnInit {
   constructor(private store: Store<AppState>) { }
 
   ngOnInit(): void {
-    this.current$ = this.store.select(state => state.counter.current)
+    this.current$ = this.store.select(selectCounterCurrent)
   }
 
   increment() {
+    this.store.dispatch(actions.countIncremented());
 
   }
   decrement() {
+    this.store.dispatch(actions.countDecremented());
 
   }
+  reset() {
+    this.store.dispatch(actions.countReset());
+  }
 }
+
+
