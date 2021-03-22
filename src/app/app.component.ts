f6b8f85a0fb16de6f39084c76c3a1361cf63c8e0
Christopher Spradlin <github@crspradlin.org>
2021-03-22 12:32:25 -0400

    added necessary components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'songs-ui';
 
-  todaysTopHits = ['Dirty Work by Steely Dan', 'I Can\'t go for that'];
-
-  addSong(txtSongEntry: HTMLInputElement):void {
-    this.todaysTopHits = [txtSongEntry.value, ...this.todaysTopHits];
-    txtSongEntry.value = '';
-    txtSongEntry.focus();
-  }
 }
