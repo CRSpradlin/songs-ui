3ca8073f8a8b7719e318bd280a9688f742fae66a
Jeffry Gonzalez <jeff@hypertheory.com>
2021-03-25 12:31:29 -0400

    before lunch Thursday

---
+++ src/app/effects/songs-effects.ts
@@ -0,0 +1,22 @@
+import { Injectable } from "@angular/core";
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { environment } from '../../environments/environment';
+import { HttpClient } from "@angular/common/http";
+
+import * as appActions from '../actions/app.actions';
+import * as songsActions from '../actions/song.actions';
+import { map } from "rxjs/operators";
+
+@Injectable()
+export class SongsEffects {
+
+  // appStarted -> loadSongs
+  appStart$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(appActions.applicationStarted),
+      map(() => songsActions.loadSongData())
+    )
+  )
+
+  constructor(private actions$: Actions, private client: HttpClient) { }
+}
