04a8a6669b3844b8bab8184eef61cdf94cc05f11
Jeffry Gonzalez <jeff@hypertheory.com>
2021-03-25 16:34:55 -0400

    done with thursday

--- src/app/actions/song.actions.ts
+++ src/app/actions/song.actions.ts
@@ -3,6 +3,7 @@ import { SongEntity } from "../reducers/songs.reducer";
 
 let id = 1;
 
+// Initiator
 export const songAdded = createAction(
   '[app songs] song added',
   ({ title, artist, recommendedBy }: { title: string, artist?: string, recommendedBy: string }) => ({
@@ -15,16 +16,29 @@ export const songAdded = createAction(
   })
 );
 
+// Success
+export const songAddedSuccessfully = createAction(
+  '[app songs] song added succesfully',
+  props<{ payload: SongEntity, oldId: string }>()
+);
 
+export const songAddedFailure = createAction(
+  '[app songs] song added failure',
+  props<{ payload: SongEntity, message: string }>()
+);
+
+// Initiator
 export const loadSongData = createAction(
   '[app songs] load the song data'
 );
 
+// Success
 export const loadSongsDataSucceeded = createAction(
   '[app songs] loading the songs succeeded',
   props<{ payload: SongEntity[] }>()
 );
 
+// Failure
 export const loadSongsFailed = createAction(
   '[app songs] loading the songs failed',
   props<{ errorMessage: string }>()
--- src/app/effects/songs-effects.ts
+++ src/app/effects/songs-effects.ts
@@ -5,10 +5,40 @@ import { HttpClient } from "@angular/common/http";
 
 import * as appActions from '../actions/app.actions';
 import * as songsActions from '../actions/song.actions';
-import { map } from "rxjs/operators";
+import { catchError, map, switchMap } from "rxjs/operators";
+import { SongEntity } from "../reducers/songs.reducer";
+import { of } from "rxjs";
 
 @Injectable()
 export class SongsEffects {
+
+  // songAdded -> (a trip to api ) => (songAddedSuccessfully | songAddedFailure)
+  addSong$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(songsActions.songAdded),
+      switchMap(originalAction => this.client.post<SongEntity>(environment.baseUrl + 'songs', {
+        title: originalAction.payload.title,
+        artist: originalAction.payload.artist,
+        recommendedBy: originalAction.payload.recommendedBy
+      }).pipe(
+        map(song => songsActions.songAddedSuccessfully({ payload: song, oldId: originalAction.payload.id })),
+        catchError(err => of(songsActions.songAddedFailure({ payload: originalAction.payload, message: 'Adding The Song Failed' })))
+      )
+
+      )
+    )
+    , { dispatch: true })
+
+  loadSongs$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(songsActions.loadSongData),
+      switchMap(() => this.client.get<GetSongsResponse>(environment.baseUrl + 'songs')
+        .pipe(
+          map(response => response.data),
+          map(data => songsActions.loadSongsDataSucceeded({ payload: data }))
+        )
+      )
+    ), { dispatch: true })
 
   // appStarted -> loadSongs
   appStart$ = createEffect(() =>
@@ -19,4 +49,8 @@ export class SongsEffects {
   )
 
   constructor(private actions$: Actions, private client: HttpClient) { }
+}
+
+interface GetSongsResponse {
+  data: SongEntity[]
 }
