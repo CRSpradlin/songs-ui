30dd1c99cdb8e1e010e259784b64317f4639d099
Jeffry Gonzalez <jeff@hypertheory.com>
2021-03-26 12:08:42 -0400

    after ui hints

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,25 +1,33 @@
-import { createSelector } from '@ngrx/store';
+import { ActionReducerMap, createSelector } from '@ngrx/store';
 import * as fromCounter from './counter.reducer';
 import * as fromSongs from './songs.reducer';
+import * as fromUiHints from './ui-hints.reducer';
+
 import { SongListModel } from '../models/songs-models';
 
 export interface AppState {
   counter: fromCounter.CounterState;
-  songs: fromSongs.SongState
+  songs: fromSongs.SongState;
+  uiHints: fromUiHints.UiHintsState
 }
 
-export const reducers = {
+export const reducers: ActionReducerMap<AppState> = {
   counter: fromCounter.reducer,
-  songs: fromSongs.reducer
+  songs: fromSongs.reducer,
+  uiHints: fromUiHints.reducer
 }
 
 const selectCounterBranch = (state: AppState) => state.counter;
 const selectSongsBranch = (state: AppState) => state.songs;
+const selectUiHintsBranch = (state: AppState) => state.uiHints;
 
 const selectSongEntityArray =
   fromSongs.adapter.getSelectors(selectSongsBranch).selectAll;
 
-
+export const selectSongsLoaded = createSelector(
+  selectUiHintsBranch,
+  b => b.songsAreLoaded
+)
 
 export const selectSongListModel = createSelector(
   selectSongEntityArray,
